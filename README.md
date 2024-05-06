1af236e3dd88a2b875553eeb88b67bff4531f89b
muslim adel <muslimadille@gmail.com>
2024-05-06 22:47:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+This project is a starting point for a Risal driver application.
+
---
+++ android/app/src/main/kotlin/com/example/risal_customer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.risal_customer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
